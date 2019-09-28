15561da6f7645fd2398fc32a6936b8c06a59f35e
Greg Griffith <griffith@temple.edu>
2019-09-28 16:58:57 -0400

    Added Login page

--- CodeLinkMVC/CodeLinkMVC/Controllers/HomeController.cs
+++ CodeLinkMVC/CodeLinkMVC/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace CodeLinkMVC.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
