66b8007cb15ddcf2ec9b794ee28d37f69aebc4f7
likhongomes <tug60499@temple.edu>
2019-09-29 01:52:38 -0400

    initial view done

---
+++ BackEndLogic/BackEndLogic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BackEndLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool homelessStatus = false
+            Console.WriteLine("Are you Homeless currently?");
+            if (homelessStatus == true)
+            {
+                Console.WriteLine("Are you satisfied with currently where you are living?");
+            } else {
+                Console.WriteLine("");
+            }
+            
+        }
+    }
+}
---
+++ iOS App/VetPlusPlus/VetPlusPlus/firstVC.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  VetPlusPlus
+//
+//  Created by Likhon Gomes on 9/29/19.
+//  Copyright © 2019 Likhon Gomes. All rights reserved.
+//
+
+import UIKit
+
+class firstVC: UIViewController {
+    
+    let loginButton = UIButton()
+    let signUpButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        loginButtonSetup()
+        signUpButtonSetup()
+    }
+    
+    func loginButtonSetup() {
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        loginButton.backgroundColor = .red
+        loginButton.setTitle("Log in", for: .normal)
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonTapped() {
+        
+    }
+    
+    func signUpButtonSetup() {
+        view.addSubview(signUpButton)
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        signUpButton.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -10).isActive = true
+        signUpButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        signUpButton.backgroundColor = .red
+        signUpButton.setTitle("Sign up in", for: .normal)
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func signUpButtonTapped() {
+        
+    }
+
+
+
+}
+
